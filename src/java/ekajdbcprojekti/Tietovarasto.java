7150b455bcac8beab57f74b1969b4f5166684be3
s1300727 <s1300727@WS1300727.bc.local>
2015-04-27 09:31:42 +0300

    paskea

--- src/java/ekajdbcprojekti/Tietovarasto.java
+++ src/java/ekajdbcprojekti/Tietovarasto.java
@@ -1,5 +1,7 @@
 package ekajdbcprojekti;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
